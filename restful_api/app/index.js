c61e13d2bd0466da2d3b58e0371c9f4f2a0403f1
pbazard <bazard.philippe@gmail.com>
2021-03-02 09:36:27 +0100

    Setup completed

--- restful_api/app/index.js
+++ restful_api/app/index.js
@@ -1,1 +1,71 @@
-console.log("Hello world!");
+/**
+ * Primary file for the API
+ */
+
+// Dependencies
+const { stat } = require('fs');
+const http = require('http');
+const { type } = require('os');
+const { StringDecoder } = require('string_decoder');
+const url = require('url');
+
+// Server
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    var queryStringObject = parsedUrl.query;
+    const method = req.method.toLocaleLowerCase();
+    var headers = req.headers;
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        var chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+
+        var data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
+        };
+
+        chosenHandler(data, function (statusCode, payload) {
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+            payload = typeof (payload) == 'object' ? payload : {};
+            var payloadString = JSON.stringify(payload);
+
+            // Return the response
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+            console.log("Return this response ", statusCode, payloadString);
+        });
+    });
+});
+
+handlers = {};
+
+handlers.notFound = (data, callback) => {
+    callback(404);
+};
+
+handlers.hello = (data, callback) => {
+    callback(406, { 'msg': 'Hello Pirple World!' });
+};
+
+
+router = {
+    'sample': handlers.sample,
+    'hello': handlers.hello
+};
+
+server.listen(3000, () => {
+    console.log("Server listening on port 3000");
+});
---
+++ restful_api/app/lib/data.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var path = requiret('path');
+
+// Container
+var lib = {};
+
+lib.create = (dir, file, data, callback) => {
+    fs.open();
+};
+
+module.exports = lib;
